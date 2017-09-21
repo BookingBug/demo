92407bf2d406d3cb46a042e51a040baf9c649380
Ehsan <ehsan.khf@gmail.com>
2017-09-21 12:26:13 +0100

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,7 @@ const gulp = require('gulp');
 const chalk = require('chalk');
 
 try {
-    // require('./node_modules/bookingbug-angular/bespoke-gulp-tasks/gulpfile')(gulp, __dirname);
-    require('../bookingbug-angular/bespoke-gulp-tasks/gulpfile')(gulp, __dirname);
+    require('./node_modules/bookingbug-angular/bespoke-gulp-tasks/gulpfile')(gulp, __dirname);
 } catch (err) {
     console.log(err);
     console.error(chalk.red('\nbookingbug-angular dependency required\n'));
