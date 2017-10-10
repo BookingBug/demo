b962169190c9a65ad0dc612442d8596019bffcc1
Damian Pudlo <pudlo.damian@gmail.com>
2017-10-10 18:30:22 +0100

    lock file

--- helpers/local_sdk.sh
+++ helpers/local_sdk.sh
@@ -2,14 +2,18 @@ if [ ! -d 'tmp' ]; then
     mkdir 'tmp'
 fi
 
-rm -rf ./node_modules ./tmp/node_modules ./yarn.lock ./tmp/yarn.lock ./tmp/package.json
+rm -rf ./tmp/package.json
 
 node ./helpers/local_sdk_prepare.js
+if [ -f 'yarn.lock' ]; then
+    cp  ./yarn.lock ./tmp/yarn.lock
+fi
 
 cd ./tmp
-yarn install
+yarn
 cd ..
 
 mv ./tmp/node_modules ./node_modules
+mv ./tmp/yarn.lock ./yarn.lock
 
 node ./helpers/local_sdk_symlink
